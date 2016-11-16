ef1d0df519a64941f01a129d2e11d70f55072c6d
Yann Henon <yannhenon@gmail.com>
2016-11-16 14:10:07 -0500

    doc string

--- SpatialPyramidPooling.py
+++ SpatialPyramidPooling.py
@@ -3,6 +3,23 @@ import keras.backend as K
 
 
 class SpatialPyramidPooling(Layer):
+    '''Spatial pyramid pooling layer for 2D inputs.
+    See Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition,
+    K. He, X. Zhang, S. Ren, J. Sun
+    # Arguments
+        pool_list: list of int
+            List of pooling regions to use. The length of the list is the number of pooling regions,
+            each int in the list is the number of regions in that pool. For example [1,2,4] would be 3
+            regions with 1, 2x2 and 4x4 max pools, so 21 outputs per feature map
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if dim_ordering='tf'.
+    # Output shape
+        2D tensor with shape:
+        `(samples, nb_filter * sum([i * i for i in pool_list])`
+    '''
 
     def __init__(self, pool_list, **kwargs):
 
