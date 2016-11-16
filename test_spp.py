eb0aa91aa48cc9e8c3ff794b1759e30fbe45336b
Yann Henon <yannhenon@gmail.com>
2016-11-16 14:43:49 -0500

    test on larger batch and multiple channels

--- test_spp.py
+++ test_spp.py
@@ -8,10 +8,13 @@ assert dim_ordering in {'tf', 'th'}, 'dim_ordering must be in {tf, th}'
 
 pooling_regions = [1, 2, 5]
 
+num_channels = 128
+batch_size = 4
+
 if dim_ordering == 'th':
-    input_shape = (1, None, None)
+    input_shape = (num_channels, None, None)
 elif dim_ordering == 'tf':
-    input_shape = (None, None, 1)
+    input_shape = (None, None, num_channels)
 
 model = Sequential()
 model.add(SpatialPyramidPooling(pooling_regions, input_shape=input_shape))
@@ -22,30 +25,33 @@ model.compile(loss='mse', optimizer='sgd')
 for img_size in [5, 8, 15]:
 
     if dim_ordering == 'th':
-        X = np.random.rand(1, 1, img_size, img_size * 2)
+        X = np.random.rand(batch_size, num_channels, img_size, img_size * 2)
         row_length = [float(X.shape[2]) / i for i in pooling_regions]
         col_length = [float(X.shape[3]) / i for i in pooling_regions]
     elif dim_ordering == 'tf':
-        X = np.random.rand(1, img_size, img_size * 2, 1)
+        X = np.random.rand(batch_size, img_size, img_size * 2, num_channels)
         row_length = [float(X.shape[1]) / i for i in pooling_regions]
         col_length = [float(X.shape[2]) / i for i in pooling_regions]
 
     Y = model.predict(X)
 
-    idx = 0
-
-    for pool_num, num_pool_regions in enumerate(pooling_regions):
-        for ix in range(num_pool_regions):
-            for jy in range(num_pool_regions):
-                x1 = int(round(ix * row_length[pool_num]))
-                x2 = int(round(ix * row_length[pool_num] + row_length[pool_num]))
-                y1 = int(round(jy * col_length[pool_num]))
-                y2 = int(round(jy * col_length[pool_num] + col_length[pool_num]))
-                if dim_ordering == 'th':
-                    m_val = np.max(X[:, :, x1:x2, y1:y2])
-                elif dim_ordering == 'tf':
-                    m_val = np.max(X[:, x1:x2, y1:y2, :])
-                np.testing.assert_almost_equal(m_val, Y[0][idx], decimal=6)
-                idx += 1
+    for batch_num in range(batch_size):
+        idx = 0
+        for pool_num, num_pool_regions in enumerate(pooling_regions):
+            for ix in range(num_pool_regions):
+                for jy in range(num_pool_regions):
+                    for cn in range(num_channels):
+                        x1 = int(round(ix * row_length[pool_num]))
+                        x2 = int(round(ix * row_length[pool_num] + row_length[pool_num]))
+                        y1 = int(round(jy * col_length[pool_num]))
+                        y2 = int(round(jy * col_length[pool_num] + col_length[pool_num]))
+                        if dim_ordering == 'th':
+                            m_val = np.max(X[batch_num, cn, x1:x2, y1:y2])
+                        elif dim_ordering == 'tf':
+                            m_val = np.max(X[batch_num, x1:x2, y1:y2, cn])
+                        np.testing.assert_almost_equal(
+                            m_val, Y[batch_num, idx], decimal=6)
+                        #print(m_val,Y[batch_num, idx])
+                        idx += 1
 
 print('Spatial pyramid pooling test passed')
